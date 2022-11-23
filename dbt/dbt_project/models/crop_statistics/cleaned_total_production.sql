f78ff9f672070ce3f8531403f5f193ba647b9670
rbekeris <bekeris.code@gmail.com>
2022-11-24 00:14:28 +0100

    cleaned first two datasets

---
+++ dbt/dbt_project/models/crop_statistics/cleaned_total_production.sql
@@ -0,0 +1,11 @@
+
+SELECT
+   index,
+   "Kultūraugi" as Crop_Name_LV,
+   year,
+   CASE 
+      WHEN value = '…' THEN Null
+      WHEN value = 'X' THEN Null
+      ELSE value
+   END AS value
+from {{ source('latvian_statistics_office', 'total_production') }}
